8cb81e41cd338497461475c002ba4f16bab99e2e
maxrchung <maxrchung@gmail.com>
2018-05-14 23:16:06 -0700

    Renamed to shape

--- DREAMSOLISTER/DREAMSOLISTER/Shape.hpp
+++ DREAMSOLISTER/DREAMSOLISTER/Shape.hpp
@@ -3,9 +3,9 @@
 #include "OsuukiSB/Vector2.hpp"
 #include "ShapeType.hpp"
 
-struct ShapeConfiguration {
-	ShapeConfiguration() {}
-	ShapeConfiguration(ShapeType pType, const Vector2& pPosition, const float pRotation, const Vector2& pScaleVector)
+struct Shape {
+	Shape() {}
+	Shape(ShapeType pType, const Vector2& pPosition, const float pRotation, const Vector2& pScaleVector)
 		: type{ pType }, position{ pPosition }, rotation{ pRotation }, scaleVector{ pScaleVector } {}
 	ShapeType type;
 	Vector2 position;
