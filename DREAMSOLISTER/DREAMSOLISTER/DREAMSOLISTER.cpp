5f7556eb8c69e232261911a2cc4d5df705237491
maxrchung <maxrchung@gmail.com>
2019-09-07 19:47:35 -0700

    Added sapphire

--- DREAMSOLISTER/DREAMSOLISTER/DREAMSOLISTER.cpp
+++ DREAMSOLISTER/DREAMSOLISTER/DREAMSOLISTER.cpp
@@ -610,14 +610,14 @@ int main() {
 		processScript("lyrics.chai", bg);
 		processScript("faces.chai", bg);
 		processScript("instruments.chai", bg);
-		//processScript("swing.chai", bg);
-		//processScript("musicSheet.chai", bg);
+		processScript("swing.chai", bg);
+		processScript("musicSheet.chai", bg);
 
-		//MusicSheet("voice.MusicSheet", 122, imageWidth, Color(0, 169, 195), "lyric.MusicSheet", false);
-		//MusicSheet("wind.MusicSheet", 0, imageWidth, Color(247, 255, 8), "", true);
-		//MusicSheet("drum.MusicSheet", -140, imageWidth, Color(255, 209, 219), "", false);
+		MusicSheet("voice.MusicSheet", 122, imageWidth, Color(0, 169, 195), "lyric.MusicSheet", false);
+		MusicSheet("wind.MusicSheet", 0, imageWidth, Color(247, 255, 8), "", true);
+		MusicSheet("drum.MusicSheet", -140, imageWidth, Color(255, 209, 219), "", false);
 
-		//setDotBackground();
+		setDotBackground();
 
 		// Blue line rectangle border
 		setBorder();
