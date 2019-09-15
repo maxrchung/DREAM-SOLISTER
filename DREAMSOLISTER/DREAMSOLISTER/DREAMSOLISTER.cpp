d4711f9052c9780eccf03a98c31208d223a85931
Naranja-Sagged <alexliuying@gmail.com>
2019-09-14 18:38:52 -0700

    Add trombone.shan

--- DREAMSOLISTER/DREAMSOLISTER/DREAMSOLISTER.cpp
+++ DREAMSOLISTER/DREAMSOLISTER/DREAMSOLISTER.cpp
@@ -628,7 +628,6 @@ int main() {
 		auto storyboardInputPath = std::ifstream("StoryboardInputPath.txt");
 		std::string path;
 		std::getline(storyboardInputPath, path);
-
 		Storyboard::Write(path);
 	return 0;
 }
