bc9bcde9f4aa61314455bcec4952f1c01dce3b24
maxrchung <maxrchung@gmail.com>
2020-01-13 22:17:42 -0800

    Removed disabled after conflicts with video

--- ShapeVideo/app/CheckLabel.jsx
+++ ShapeVideo/app/CheckLabel.jsx
@@ -4,24 +4,18 @@ import PropTypes from 'prop-types';
 export default class CheckLabel extends React.Component {
   static propTypes = {
     checked: PropTypes.bool.isRequired,
-    disabled: PropTypes.bool,
     label: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired
   };
 
-  static defaultProps = {
-    disabled: false
-  };
-
   render() {
-    const { checked, disabled, label, name, onChange } = this.props;
+    const { checked, label, name, onChange } = this.props;
     return (
       <div className="form-check">
         <label className="form-check-label" htmlFor={name}>
           <input
             className="form-check-input"
-            disabled={disabled}
             type="checkbox"
             name={name}
             checked={checked}
--- ShapeVideo/app/Slider.jsx
+++ ShapeVideo/app/Slider.jsx
@@ -3,20 +3,14 @@ import PropTypes from 'prop-types';
 
 export default class Slider extends React.Component {
   static propTypes = {
-    disabled: PropTypes.bool,
     value: PropTypes.number.isRequired,
     onChange: PropTypes.func.isRequired
   };
 
-  static defaultProps = {
-    disabled: false
-  };
-
   render() {
-    const { disabled, value, onChange } = this.props;
+    const { value, onChange } = this.props;
     return (
       <input
-        disabled={disabled}
         type="range"
         className="custom-range"
         min="0"
