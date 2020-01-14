f985fa95ed8eb59e65d7f2f54e8bf40ea1aa4dbc
maxrchung <maxrchung@gmail.com>
2020-01-13 20:59:00 -0800

    Fixed tests to not rely on video ref

--- ShapeVideo/app/test/Shape.test.js
+++ ShapeVideo/app/test/Shape.test.js
@@ -6,26 +6,26 @@ import ShapeType from '../ShapeType';
 describe('type', () => {
   it('sets rectangle', () => {
     const shape = shallow(<Shape type={ShapeType.Rectangle} />);
-    expect(shape.find('img').props().src).toBe('assets/Rectangle.png');
+    expect(shape.instance().getSrcPath()).toBe('assets/Rectangle.png');
   });
 
   it('sets triangle', () => {
     const shape = shallow(<Shape type={ShapeType.Triangle} />);
-    expect(shape.find('img').props().src).toBe('assets/Triangle.png');
+    expect(shape.instance().getSrcPath()).toBe('assets/Triangle.png');
   });
 
   it('sets circle', () => {
     const shape = shallow(<Shape type={ShapeType.Circle} />);
-    expect(shape.find('img').props().src).toBe('assets/Circle.png');
+    expect(shape.instance().getSrcPath()).toBe('assets/Circle.png');
   });
 
   it('sets semicircle', () => {
     const shape = shallow(<Shape type={ShapeType.Semicircle} />);
-    expect(shape.find('img').props().src).toBe('assets/Semicircle.png');
+    expect(shape.instance().getSrcPath()).toBe('assets/Semicircle.png');
   });
 
   it('sets none', () => {
     const shape = shallow(<Shape type={ShapeType.None} />);
-    expect(shape.find('img').props().src).toBe('');
+    expect(shape.instance().getSrcPath()).toBe('');
   });
 });
